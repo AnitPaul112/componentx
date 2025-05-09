bb127778d2a2fef87e859f983ed90134738ec988
Trisheeta Jahan Azad <trisheeta.jahan.azad@g.bracu.ac.bd>
2025-05-09 06:25:25 +0600

    created blogController, blog.php and blade page of blog

--- database/migrations/2025_01_01_000003_create_blog_table copy.php
+++ database/migrations/2025_01_01_000003_create_blog_table copy.php
@@ -4,21 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBlogTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::create('blog', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('username');
-            $table->text('blog_text');
+            $table->text('content');
             $table->timestamps();
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('blog');
     }
-}
+}; 
